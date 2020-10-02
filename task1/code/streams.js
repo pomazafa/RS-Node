6e2c9d1692327dd0d9752b7e7edbb56bd568b366
Polina <p.duben@mail.ru>
2020-10-02 15:31:08 +0300

    fix: add process.exit after invalid output file parameter

--- task1/code/streams.js
+++ task1/code/streams.js
@@ -1,30 +1,31 @@
-const fs = require('fs');
+const fs = require("fs");
 
 module.exports = (params) => {
-    let input;
-    let output;
+  let input;
+  let output;
 
-    if (params.input) {
-        try {
-            fs.accessSync(params.input, fs.constants.R_OK);
-            input = fs.createReadStream(params.input);
-        } catch (err) {
-            console.error('Error: Input file is not readable or does not exists');
-            process.exit(9);
-        }
+  if (params.input) {
+    try {
+      fs.accessSync(params.input, fs.constants.R_OK);
+      input = fs.createReadStream(params.input);
+    } catch (err) {
+      console.error("Error: Input file is not readable or does not exists");
+      process.exit(9);
     }
+  }
 
-    if (params.output) {
-        try {
-            fs.accessSync(params.output, fs.constants.W_OK);
-            output = params.output
-        } catch (err) {
-            console.error('Error: Output file is not writable or does not exists');
-        }
+  if (params.output) {
+    try {
+      fs.accessSync(params.output, fs.constants.W_OK);
+      output = params.output;
+    } catch (err) {
+      console.error("Error: Output file is not writable or does not exists");
+      process.exit(9);
     }
+  }
 
-    return {
-        input,
-        output
-    };
-}
+  return {
+    input,
+    output,
+  };
+};
